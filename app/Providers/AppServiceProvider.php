40a1733ab55f1c1aa50c9fe2fb10424b06352f4c
ukrasouski <ukrasouski@ambrygen.com>
2019-04-16 11:43:10 -0700

    working sam

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         // Make sure the directory for compiled views exist
         if (! is_dir(config('view.compiled'))) {
             mkdir(config('view.compiled'), 0755, true);
